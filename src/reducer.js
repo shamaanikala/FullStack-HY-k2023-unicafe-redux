3930bc5a775efff8b0824f7986c5b402d24097ef
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-04 15:50:51 +0300

    T6.1 reducer OK toiminnallisuus

--- src/reducer.js
+++ src/reducer.js
@@ -10,7 +10,7 @@ const counterReducer = (state = initialState, action) => {
     case 'GOOD':
       return { ...state, good: state.good + 1 }
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
       return state
     case 'ZERO':
